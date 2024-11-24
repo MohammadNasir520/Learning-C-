68cae4bd089fa6945fd5d639588482a4e21342a4
MohammadNasir520 <nasirahsan520@gmail.com>
2024-11-24 16:27:00 +0600

    learned setprecision

--- learningC++.cpp
+++ learningC++.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
 int main()
 {
     // std::cout << "Hello World!";
 
-    int x = 10;
+    // int x = 10;
 
-    char c = 'a';
-    double b = 10.34;
-    std::cout << x << ' ' << c << b << std::endl;
+    // char c = 'a';
+    // double b = 10.34;
+    // std::cout << x << ' ' << c << b << std::endl;
 
+    // int x;
+    // std::cin >> x;
+
+    // std::cout << x + x;
+    // char x;
+    // cin >> x;
+    // // type casting
+    // cout << (int)x;
+
+    // input  and EOF
+
+    // int x;
+
+    // while (cin >> x)
+    // {
+    //     cout << x;
+    // }
+
+    // double a = 18.2434343;
+
+    // cout <<  a;
+
+    double a = 18.2434343;
+
+    cout << fixed << setprecision(2) << a << endl;
     return 0;
 }
