d5712819be4fe8f66d1561ac4e44cf4d6afd1be0
MohammadNasir520 <nasirahsan520@gmail.com>
2024-11-24 16:02:47 +0600

    learned print of c++

--- learningC++.cpp
+++ learningC++.cpp
@@ -4,7 +4,10 @@ int main()
     // std::cout << "Hello World!";
 
     int x = 10;
-    std::cout << x;
+
+    char c = 'a';
+    double b = 10.34;
+    std::cout << x << ' ' << c << b << std::endl;
 
     return 0;
 }
